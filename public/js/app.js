5cd54e33404c2addb6bcb5e158db71e3e1bd9e56
DmitryRomaniuk <Kanumowa@gmail.com>
2016-07-17 23:58:38 +0300

    Switching from list and deleting task and comments

--- public/js/app.js
+++ public/js/app.js
@@ -86,109 +86,110 @@ var Footer = React.createClass({
 
 var ListofProjects = React.createClass({
     getInitialState: function () {
-        console.log(this.props.list);
         var list = this.props.list;
-        return {list};
+        return {list: list, selectedProject: 0, selectedTask: 0};
+    },
+    checkboxClick: function (i, e) {
+        this.state.list[this.state.selectedProject].listTask[i].isChecked = e.target.checked;
+        this.setState({list: this.state.list});
+    },
+    selectTask: function (i, e) {
+        this.setState({selectedTask: i});
+    },
+    selectProject: function (i, e) {
+        this.setState({selectedProject: i});
+    },
+    eraseComment: function (i,e) {
+        console.log(this.state.list);
+        this.state.list[this.state.selectedProject].listTask[this.state.selectedTask].listDescriptions.splice(i,1);
+        this.setState({list: this.state.list});
+    },
+    eraseTask: function (i,e) {
+        console.log(this.state.list);
+        this.state.list[this.state.selectedProject].listTask.splice(i,1);
+        this.setState({list: this.state.list});
+    },
+    editClick: function (e) {
+        console.log(e);
+        console.log('edit');
+        this.setState({list: this.state.list}, console.log('edit'));
+    },
+    addTask: function (e) {
+        console.log('edit');
     },
     render: function () {
         return (
-            <article className="col-xs-4 project-left">
-                <div className="list-group">
-                    {this.state.list.map((task, i)=>
-                        <div className={(i==0)? "list-group-item active":"list-group-item"} key={Math.random()}>
-                            <div className="list-group-item-wrap">
-                                <span> {task.nameProject}</span>
-                                <span className="list-group-item-delete">X</span>
-                                <i className="fa fa-pencil" aria-hidden="true"/>
-                            </div>
-                        </div>)}
-                </div>
-            </article>
-
-        )
-    }
-});
-var ListofTasks = React.createClass({
-    getInitialState: function () {
-        var listTasks = this.props.list;
-        //console.log(listTasks);
-        return {listTasks};
-    },
-    checkboxClick: function () {
-        this.setState({listTasks:this.state.listTasks}, console.log('click input'));
-    },
-    eraseClick: function () {
-        this.setState({listTasks:this.state.listTasks}, console.log('erase'));
-    },
-    editClick: function () {
-        this.setState({listTasks:this.state.listTasks}, console.log('edit'));
-    },
-    render: function () {
-        return (
-            <article className="col-xs-4 task-center">
-                <div className="list-group">
-                    {this.state.listTasks.map((task, i)=>
-                        <div className={(i==0)? "list-group-item active":"list-group-item"} key={Math.random()}>
-                            <div className="list-group-item-wrap">
-                                <input type="checkbox"
-                                       checked={task.isChecked}
-                                       onChange={this.state.checkboxClick}
-                                />
-                                <span> {task.name}</span>
+            <div>
+                <article className="col-xs-4 project-left">
+                    <div className="list-group">
+                        {this.state.list.map((task, i)=>
+                            <div className={(i==this.state.selectedProject)? "list-group-item active":"list-group-item"}
+                                 onClick={this.selectProject.bind(this,i)}
+                                 key={i}>
+                                <div className="list-group-item-wrap">
+                                    <span> {task.nameProject}</span>
+                                    <span className="list-group-item-delete">X</span>
+                                    <i className="fa fa-pencil" aria-hidden="true"/>
+                                </div>
+                            </div>)}
+                    </div>
+                </article>
+                <article className="col-xs-4 task-center">
+                    <div className="list-group">
+                        {this.state.list[this.state.selectedProject].listTask.map((task, i)=>
+                            <div className={(i==this.state.selectedTask)? "list-group-item active":"list-group-item"}
+                                 key={i+100}
+                                 onClick={this.selectTask.bind(this,i)}>
+                                <div className="list-group-item-wrap">
+                                    <input type="checkbox"
+                                           checked={task.isChecked}
+                                           onChange={this.checkboxClick.bind(this,i)}
+                                    />
+                                    <span> {task.name}</span>
                                 <span className="list-group-item-delete"
-                                      onClick={this.state.eraseClick}>X</span>
-                                <i className="fa fa-pencil"
-                                   aria-hidden="true"
-                                   onClick={this.state.editClick}/>
-                            </div>
-                        </div>)}
-                    <div className="button-create-entry">
-                        <a href="#" className="btn btn-info">Create task</a>
+                                      onClick={this.eraseTask.bind(this,i)}>X</span>
+                                    <i className="fa fa-pencil"
+                                       aria-hidden="true"
+                                       onClick={this.editClick.bind(this,i)}/>
+                                </div>
+                            </div>)}
+                        <div className="button-create-entry">
+                            <button className="btn btn-info" onClick={this.addTask}>Create task</button>
+                        </div>
                     </div>
-                </div>
-            </article>
+                </article>
+                <article className="col-xs-4 description-right">
+                    <div className="list-group">
+                        {this.state.list[this.state.selectedProject]
+                            .listTask[this.state.selectedTask].listDescriptions.map((descr, i)=>
+                            <div className={(i==0)? "list-group-item active":"list-group-item"} key={Math.random()}>
+                                <div className="list-group-item-wrap">
+                                    <span> {descr}</span>
+                                    <span className="list-group-item-delete"
+                                            onClick={this.eraseComment.bind(this,i)}>X</span>
+                                    <i className="fa fa-pencil" aria-hidden="true"/>
+                                </div>
+                            </div>
+                        )}
+                        <div className="button-create-entry">
+                            <button href="#" className="btn btn-info">Add comment</button>
+                        </div>
+                    </div>
+                </article>
+            </div>
         )
     }
 });
 
-var ListofDescriptions = React.createClass({
-    getInitialState: function () {
-        var allDescriptions = this.props.list;
-        return {allDescriptions};
-    },
-    render: function () {
-        return (
-            <article className="col-xs-4 description-right">
-                <div className="list-group">
-                    {this.state.allDescriptions.map((descr, i)=>
-                        <div className={(i==0)? "list-group-item active":"list-group-item"} key={Math.random()}>
-                            <div className="list-group-item-wrap">
-                                <span> {descr}</span>
-                                <span className="list-group-item-delete">X</span>
-                                <i className="fa fa-pencil" aria-hidden="true"/>
-                            </div>
-                        </div>
-                    )}
-                    <div className="button-create-entry">
-                        <a href="#" className="btn btn-info">Add comment</a>
-                    </div>
-                </div>
-            </article>
-        )
-    }
-});
 
 var Main = React.createClass({
     getInitialState: function () {
-        //console.log(this.props.list);
-        return null;
+        return {list: this.props.list};
     },
     render: function () {
         return (
             <div className="row">
-                <ListofProjects list={this.props.list}/>
-                <ListofTasks list={this.props.list[0].listTask}/>
-                <ListofDescriptions list={this.props.list[0].listTask[0].listDescriptions}/>
+                <ListofProjects list={this.state.list}/>
             </div>
         )
     }
@@ -197,7 +198,6 @@ var Main = React.createClass({
 
 var App = React.createClass({
     getInitialState: function () {
-        //console.log(this.props);
         return {list: this.props.list};
     },
     render: function () {
